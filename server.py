d28ef4fa4677e87147e8c7a955dd50f03319ec9f
Danglebary <halfblowncontact@gmail.com>
2021-08-22 19:27:12 -0700

    first initial commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Response
+from api import app, genre_list, jsonify
+from flask_cors import cross_origin
+
+@app.route('/api', methods=["GET"])
+def hello_world():
+    return "Hello from the backend"
+
+@app.route('/genres', methods=["GET"])
+@cross_origin()
+def genres():
+    print('ENDPOINT HAS BEEN HIT')
+    genres = jsonify(genres=genre_list)
+    return genres
